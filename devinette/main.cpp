f3218ec2be07a73e7e948f9b839323436f826cd9
d'ALMEIDA Régis <157544686+jordanWannaCode@users.noreply.github.com>
2025-02-02 16:30:08 +0000

    Add files via upload

---
+++ devinette/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    cout << "*** DEVINETTE ***" << endl;
+    unsigned int nbMystere;
+    unsigned int level);
+    int nMax;
+
+    cout << "Choisissez un niveau" <<endl;
+    cout << " 1. Facile(0-100)" <<endl;
+    cout << " 2. Moyen(0-500)" <<endl;
+    cout << " 3. Difficile(0-1000)" <<endl;
+
+    switch (level)
+        {
+        case 1:
+            nMax=100;
+            break;
+        case 2:
+            nMax=100;
+            break;
+        case 3:
+            nMax=100;
+            break;
+        default:
+            cout<<"Choisissez une niveau valide"<< endl;
+        }
+
+
+
+
+    return 0;
+}
